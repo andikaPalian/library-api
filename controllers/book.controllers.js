51f22aa57af2984cfeaa4a6800ec46cef509115e
andikaPalian <andika12des@gmail.com>
2024-12-12 11:24:41 +0800

    menambahkan dan menyelesaikan book controllers dan routes

--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -29,4 +29,83 @@ const addBook = async (req, res) => {
     };
 };
 
-module.exports = {addBook};
+const getAll = async (req, res) => {
+    try {
+        const { genre } = req.query;
+        const filters = {};
+        if (genre) {
+            filters.genre = genre;
+        };
+        const books = await Book.find(filters);
+        if (!books || books.length === 0) {
+            return res.status(404).json({message: genre ? `Book with genre ${genre} not found` : "Books Not Found"});
+        };
+        res.status(200).json({books});
+    } catch (error) {
+        console.error("Error getting books:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const getSingleBook = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(404).json({message: "No book with that ID"});
+        };
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({message: "Book Not Found"});
+        };
+        res.status(200).json({book});
+    } catch (error) {
+        console.error("Error getting book:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const updateBook = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "No book with that ID"});
+        };
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({message: "Book Not Found"});
+        };
+        const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json({message: "Book updated successfully", data: updateBook});
+    } catch (error) {
+        console.error("Error updating book:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const deleteBook = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "No book with that ID"});
+        };
+        const book = await Book.findByIdAndDelete(req.params.id);
+        if (!book) {
+            return res.status(404).json({message: "Book Not Found"});
+        };
+        res.status(200).json({message: "Book deleted successfully"});
+    } catch (error) {
+        console.error("Error deleting book:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+module.exports = {addBook, getAll, getSingleBook, updateBook, deleteBook};
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { addBook } = require("../controllers/book.controllers");
+const { addBook, getAll, getSingleBook, updateBook, deleteBook } = require("../controllers/book.controllers");
 const router = express.Router();
 
 router.post("/add", addBook);
+router.get("/get", getAll);
+router.get("/get/:id", getSingleBook);
+router.put("/update/:id", updateBook);
+router.delete("/delete/:id", deleteBook);
 
 module.exports = router;
